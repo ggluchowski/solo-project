9c6e89414b939fc6869d6467f8337d9f3c631c99
Grzegorz Głuchowski <ggrzegorz89@gmail.com>
2021-08-12 01:50:23 +0200

    Add responsive menu

--- src/js/script.js
+++ src/js/script.js
@@ -10,74 +10,16 @@
       personalData: '.section-personal-data',
       payout: '.section-payout',
       chartEarnings: 'earningsChart',
+      menuQuite: 'menu-quite',
+      hamburger: '.hamburger',
+      panel: '.panel',
+      hamburgerMenu: '.menu-left',
+      clickMenu: '.menu-list',
+      topMenuBar: '.top-menu-bar',
+      sidebarHeight: '.sidebar',
     }
 
   };
-
-  class Menu {
-    constructor() {
-      const thisMenu = this;
-      thisMenu.getElements();
-      thisMenu.initActions();
-    }
-
-    getElements() {
-      const thisMenu = this;
-
-      thisMenu.dom = {};
-      thisMenu.dom.general = document.querySelector(select.containerOf.general);
-      thisMenu.dom.banners = document.querySelector(select.containerOf.banners);
-      thisMenu.dom.links = document.querySelector(select.containerOf.links);
-      thisMenu.dom.personalData = document.querySelector(select.containerOf.personalData);
-      thisMenu.dom.payout = document.querySelector(select.containerOf.payout);
-
-    }
-
-    initActions() {
-
-    }
-
-
-
-  }
-
-  // Modale
-
-  // ZAMYKANIE
-  function closeModal() {
-    document.getElementById('overlay').classList.remove('show');
-  }
-
-  // zamkniecie za pomoca btn
-  document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-      e.preventDefault();
-      closeModal();
-    });
-  });
-
-  // zamkniecie za pomoca klikniecia na tlo
-  document.querySelector('#overlay').addEventListener('click', function (e) {
-    if (e.target === this) {
-      closeModal();
-    }
-  });
-
-  // zamkniecie za pomoca przycisku ESC
-  document.addEventListener('keyup', function (e) {
-    if (e.keyCode === 27) {
-      closeModal();
-    }
-  });
-
-  // OTWIERANIE
-  function openModal(modal) {
-    document.querySelectorAll('#overlay > *').forEach(function (modal) {
-      modal.classList.remove('show');
-    });
-    document.querySelector('#overlay').classList.add('show');
-    document.querySelector(modal).classList.add('show');
-  }
 
   // WYKRES
   const ctx = document.getElementById(select.containerOf.chartEarnings).getContext('2d');
@@ -115,19 +57,144 @@
     },
   });
 
-  function toggleMenu(visible) {
-    document.querySelector('.xxx').classList.toggle('show', visible);
+  // MODALE
+
+  // ZAMYKANIE
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show');
   }
 
-  document.querySelector('.hamburger').addEventListener('click', function (e) {
-    e.preventDefault();
-    toggleMenu();
+  // zamkniecie za pomoca btn
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      closeModal();
+    });
   });
+
+  // zamkniecie za pomoca klikniecia na tlo
+  document.querySelector('#overlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+      closeModal();
+    }
+  });
+
+  // zamkniecie za pomoca przycisku ESC
+  document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+      closeModal();
+    }
+  });
+
+
+  class Menu {
+    constructor() {
+      const thisMenu = this;
+      thisMenu.getElements();
+      thisMenu.initActions();
+      thisMenu.listListener();
+    }
+
+    getElements() {
+      const thisMenu = this;
+
+      thisMenu.dom = {};
+      thisMenu.dom.general = document.querySelector(select.containerOf.general);
+      thisMenu.dom.banners = document.querySelector(select.containerOf.banners);
+      thisMenu.dom.links = document.querySelector(select.containerOf.links);
+      thisMenu.dom.personalData = document.querySelector(select.containerOf.personalData);
+      thisMenu.dom.payout = document.querySelector(select.containerOf.payout);
+
+      //modal-quite
+      thisMenu.dom.modalQuite = document.getElementById(select.containerOf.menuQuite);
+
+      //hamburger
+      thisMenu.dom.hamburger = document.querySelector(select.containerOf.hamburger);
+
+      //hamburger container - menu
+      thisMenu.dom.hamburgerMenu = document.querySelector(select.containerOf.hamburgerMenu);
+
+      //hamburger container - menuTOP
+      thisMenu.dom.topMenuBar = document.querySelector(select.containerOf.topMenuBar);
+
+      //hamburger - sidebar height
+      thisMenu.dom.sidebarHeight = document.querySelector(select.containerOf.sidebarHeight);
+
+      //panele
+      thisMenu.dom.panels = document.querySelectorAll(select.containerOf.panel);
+
+      //menu
+      thisMenu.dom.clickMenu = document.querySelectorAll(select.containerOf.clickMenu);
+
+    }
+
+    initActions() {
+      const thisMenu = this;
+
+      thisMenu.dom.hamburger.addEventListener('click', function (e) {
+        e.preventDefault();
+        thisMenu.toggleMenu();
+      });
+
+      thisMenu.dom.modalQuite.addEventListener('click', function (e) {
+        e.preventDefault();
+        thisMenu.openModal('#quit');
+      });
+    }
+
+    listListener() {
+      const thisMenu = this;
+      const list = thisMenu.dom.clickMenu;
+
+      for (let link of list) {
+        link.addEventListener('click', function (e) {
+          e.preventDefault();
+          const click = this;
+          const activeLinks = document.querySelectorAll('.menu-left li.active');
+
+          for (let activeLink of activeLinks) {
+            activeLink.classList.remove('active');
+          }
+
+          click.classList.add('active');
+          const activeSections = document.querySelectorAll('.panel.show');
+
+          for (let activeSection of activeSections) {
+            activeSection.classList.remove('show');
+          }
+
+          const hrefAttribute = click.querySelector('a').getAttribute('href');
+
+          const correctSection = document.querySelector(hrefAttribute);
+
+          if (hrefAttribute !== '#blank')
+            correctSection.classList.add('show');
+
+        });
+      }
+    }
+
+    toggleMenu(visible) {
+      const thisMenu = this;
+      thisMenu.dom.hamburgerMenu.classList.toggle('show', visible);
+      thisMenu.dom.topMenuBar.classList.toggle('show', visible);
+      thisMenu.dom.sidebarHeight.classList.toggle('sidebar-height', visible);
+    }
+    // OTWIERANIE Pop-upa
+    openModal(modal) {
+      //const thisMenu = this;
+      document.querySelectorAll('#overlay > *').forEach(function (modal) {
+        modal.classList.remove('show');
+      });
+      document.querySelector('#overlay').classList.add('show');
+      document.querySelector(modal).classList.add('show');
+    }
+
+  }
+
 
   const app = new Menu();
   console.log(app);
   console.log(chart);
-  openModal('#quit');
-
 
 }
